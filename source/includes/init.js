2e1e9ad28fca258c805c844f3a875229049d1841
VampiricDesire <kapsonfire@googlemail.com>
2015-05-18 14:26:09 +0200

    +einbinden der ModulKlasse +einbinden des ChannelTop Moduls

---
+++ source/includes/init.js
@@ -0,0 +1,16 @@
+/**
+ * @file Diese Datei bindet alle Extensions, Klassen und Module ein
+ * @author KnuddelsTools
+ * @copyright KnuddelsTools
+ * @link https://github.com/KnuddelsTools KnuddelsTools on Github
+ *
+ * @license LGPL
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+
+require("includes/Class.Module.js");
+
+
+
+require("includes/modules/Module.ChannelTop.js");
