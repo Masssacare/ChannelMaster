36b77edee270259b41f5d7ee66fd038b46175977
Kapsonfire <kapsonfire@gmx.de>
2015-05-19 18:25:18 +0200

    ModuleManager fixed project link

--- source/includes/modules/Module.ModuleManager.js
+++ source/includes/modules/Module.ModuleManager.js
@@ -71,7 +71,7 @@ ModuleManager.prototype.onUserJoined = function(user) {
           "°BB°_"+appName+"_°r° läuft in der Version _" + appVersion + "_" +
           "°#°_Registrierte Module:_ " + App.modules.registered.join(", ") +
           "°#°_Aktivierte Module:_ " +  activated.join(", ") +
-          "°#°_Hilfe & Anleitung: °BB>"+App.projectURL+"<°°r°_"
+          "°#°_Hilfe & Anleitung: °BB>"+App.projectURL+"|"+App.projectURL+"<°°r°_"
       );
   }
 };
