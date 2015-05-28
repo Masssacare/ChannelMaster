5bd96a74941325cb3cbfc3de471c4bbb424f6df6
DdvoID <ddvoid@web.de>
2015-05-28 20:16:49 +0200

    Added LastOnline

--- source/includes/modules/Module.Lastonline.js
+++ source/includes/modules/Module.Lastonline.js
@@ -1,3 +1,56 @@
 /**
- * Created by user on 28.05.15.
+ * @file Diese Datei definiert das LastOnline Module
+ * @author KnuddelsTools
+ * @author DdvOiD
+ * @copyright KnuddelsTools
+ * @link https://github.com/KnuddelsTools KnuddelsTools on Github
+ *
+ * @license LGPL
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
  */
+
+/**
+ * @class LastOnline
+ * @extends Module
+ * @constructor
+ */
+function LastOnline() {
+    App.registerModule(this);
+};
+LastOnline.prototype = new Module;
+LastOnline.prototype.constructor = LastOnline;
+
+LastOnline.prototype.onActivated = function() {
+    this.registerCommand("lastonline", this.cmdLastOnline);
+};
+
+LastOnline.prototype.onDeactivated = function() {
+    this.unregisterCommand("lastonline");
+};
+
+/**
+ *
+ * @param {User} user
+ */
+
+LastOnline.prototype.onUserJoined = function(user) {
+    var persis = user.getPersistence();
+    persis.setNumber("mLastOnline_join", Date.now());
+};
+/**
+ *
+ * @param {User} user
+ * @param {string} params
+ * @param {string} func
+ */
+LastOnline.prototype.cmdLastOnline = function(user, params, func) {
+    var message = "°#°°BB18°_Folgende User waren zuletzt hier im Channel:§";
+    var entries = UserPersistenceNumbers.getSortedEntries("mLastOnline_join", { ascending: false, count: 30 });
+    for(var key in entries){
+        var entry = entries[key];
+        message += "°#°"+ entry.getRank() +" - "+ entry.getUser().getProfileLink() + " ("+ new Date (entry.getValue()).toGermanString() + ")";
+    }
+    user.sendPrivateMessage(message);
+};
+
+LastOnline.self = new LastOnline;
